c37dfb793997c705f9680003c02cec67990d1a57
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2022-01-14 21:40:45 +0900

    check range

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
-phpinfo();
+// echo $_SERVER['HTTP_USER_AGENT'];
+
+// $food = 'cake';
+
+
+// $return_value = match($food) {
+//     'apple'->'This food is an apple',
+//     'bar''This food is a bar',
+//     'cake'=>'This food is a cake',
+// };
+
+// var_dump($return_value);
+
+foreach (range(100, 1) as $number) {
+    echo $number;
+    echo "\n";
+}
